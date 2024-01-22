afbb7a203a2a63634a11a9a2a0a0141f88493195
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-01-22 02:49:32 -0300

    feature/ECO-10 - Tela inicial de gestão de contrato

---
+++ app/Models/SvnSegGeoV2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SvnSegGeoV2 extends Model
+{
+    use HasFactory;
+
+    protected $table = 'svn_seg_geo_v2';
+}
