655013cf9a1a70e047b0021c6e9438e289977c78
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-01-26 02:04:28 -0300

    feat: Gestão de contratos - ECO-12

--- app/Domain/Contrato/GestaoContrato/Controller/CreateContratoController.php
+++ app/Domain/Contrato/GestaoContrato/Controller/CreateContratoController.php
@@ -2,7 +2,11 @@
 
 namespace App\Domain\Contrato\GestaoContrato\Controller;
 
+use App\Models\Contrato;
+use App\Models\ContratoSituacao;
 use App\Models\ContratoTipo;
+use App\Models\Rodovia;
+use App\Models\Uf;
 use App\Shared\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
@@ -10,12 +14,29 @@ use Inertia\Inertia;
 
 class CreateContratoController extends Controller
 {
-    public function create()
+    public function create(Contrato|null $contrato)
     {
-        $contratoTipos = Cache::rememberForever('contrato_tipos', fn () => ContratoTipo::all());
+        $ufs = Cache::rememberForever('ufs', fn () => Uf::all());
+        $rodovias = Cache::rememberForever('rodovias', fn () => Rodovia::all());
+        $tipos = Cache::rememberForever('contrato_tipos', fn () => ContratoTipo::all());
+        $situacoes = Cache::rememberForever('contrato_situacoes', fn () => ContratoSituacao::all());
+
+        if ($contrato) {
+            $contrato->load([
+                'situacao',
+                'tipo',
+                'trechos',
+                'trechos.uf',
+                'trechos.rodovia'
+            ]);
+        }
 
         return Inertia::render('Contrato/GestaoContrato/Form', [
-            'contratoTipos' => $contratoTipos
+            'ufs' => $ufs,
+            'rodovias' => $rodovias,
+            'tipos' => $tipos,
+            'situacoes' => $situacoes,
+            'contrato' => $contrato
         ]);
     }
 }
