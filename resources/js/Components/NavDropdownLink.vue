87cd5ab008a8ecbe64f48e168231bb3a4ec8aa70
Lucas Emanoel <luscaemanoel@gmail.com>
2024-02-02 12:55:24 -0300

    feat: Menu selecionado

--- resources/js/Components/NavDropdownLink.vue
+++ resources/js/Components/NavDropdownLink.vue
@@ -1,18 +1,30 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import { computed } from "vue";
 
-defineProps({
+const props = defineProps({
     routeName: { type: String },
-    routeParam: { type: String },
     title: { type: String },
-    activeOnRoutePrefix: { type: String }
+    activeOnRoutePrefix: { type: String },
+    routeParam: { type: String, default: null },
+    paramName: { type: String, default: null },
 })
 
+const isActive = computed(() => {
+
+    const isCurrent = route().current(props.activeOnRoutePrefix);
+
+    if (props.routeParam === null) {
+        return isCurrent
+    }
+
+    return isCurrent && props.routeParam == route().params[props.paramName]
+
+})
 </script>
 
 <template>
-    <Link class="dropdown-item" v-if="can(routeName)" :class="{ active: route().current(activeOnRoutePrefix) }"
-        :href="route(routeName, routeParam)">
+    <Link class="dropdown-item" v-if="can(routeName)" :class="{ active: isActive }" :href="route(routeName, routeParam)">
     {{ title }}
     </Link>
 </template>
