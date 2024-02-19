24c06288558c1fac1f88d548d3c0d160faeffe92
knancia <oliveira.augusto@outlook.com>
2024-02-19 17:17:18 -0300

    Equivalente a ECO-18: Finalização do Cadastro / Edição do forumlario de Licenças

--- app/Domain/Licenca/Controller/CreateLicencaController.php
+++ app/Domain/Licenca/Controller/CreateLicencaController.php
@@ -3,20 +3,22 @@
 namespace App\Domain\Licenca\Controller;
 
 use App\Shared\Http\Controllers\Controller;
+use App\Models\Licenca;
 use App\Models\LicencaTipo;
 use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
 
 class CreateLicencaController extends Controller
 {
-    public function index()
+    public function index(Licenca|null $licenca)
     {
         $tipos = Cache::rememberForever('licenca_tipo', function () {
             return LicencaTipo::select('id', 'sigla', 'nome')->get();
         });
         
         return Inertia::render('Licenca/Form', [
-            'tipos' => $tipos
+            'tipos'     => $tipos,
+            'licenca'   => $licenca
         ]);
     }
 }
