88236eb8f4a9167b4d53e6f70736a9b90116b594
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-03-21 02:26:13 -0300

    feat: Recurso equipamento - ECO-58

--- resources/js/Pages/Contrato/Contratada/Navbar.vue
+++ resources/js/Pages/Contrato/Contratada/Navbar.vue
@@ -11,40 +11,40 @@ const props = defineProps({
 })
 </script>
 
-
-
 <template>
-  <div class="d-flex">
-    <div class="col-2">
-      <ul class="navbar-nav">
-        <!-- Dashboard -->
-        <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Dados Gerais"
-          :icon="IconLayoutDashboard" />
+  <div class="card card-body space-y-3">
+    <div class="d-flex">
+      <div class="col-2">
+        <ul class="navbar-nav">
+          <!-- Dashboard -->
+          <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Dados Gerais"
+            :icon="IconLayoutDashboard" />
 
-        <!-- Contratos -->
-        <NavDropdown prefix="contratos.*" title="Recuros" :icon="IconLayoutDashboard">
-          <!-- Gestão Ambiental -->
-          <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="1"
-            active-on-route-prefix="contratos.gestao.*" title="RH" />
+          <!-- Contratos -->
+          <NavDropdown prefix="contratos.*" title="Recuros" :icon="IconLayoutDashboard">
+            <!-- Gestão Ambiental -->
+            <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="1"
+              active-on-route-prefix="contratos.gestao.*" title="RH" />
 
-          <!-- Estudo Ambiental -->
-          <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="2"
-            active-on-route-prefix="contratos.gestao.*" title="Equipamentos" />
+            <!-- Equipamentos -->
+            <NavDropdownLink route-name="contratos.contratada.recurso.equipamento.index" param-name="contrato"
+              :route-param="contrato.id" title="Equipamentos" />
 
-          <!-- Regularização Ambiental -->
-          <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="3"
-            active-on-route-prefix="contratos.gestao.*" title="Veículos" />
-        </NavDropdown>
+            <!-- Regularização Ambiental -->
+            <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="3"
+              active-on-route-prefix="contratos.gestao.*" title="Veículos" />
+          </NavDropdown>
 
-        <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Serviços"
-          :icon="IconLayoutDashboard" />
+          <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Serviços"
+            :icon="IconLayoutDashboard" />
 
-        <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Cronograma"
-          :icon="IconLayoutDashboard" />
-      </ul>
-    </div>
-    <div class="col">
-      <slot name="body" />
+          <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Cronograma"
+            :icon="IconLayoutDashboard" />
+        </ul>
+      </div>
+      <div class="col">
+        <slot name="body" />
+      </div>
     </div>
   </div>
 </template>
