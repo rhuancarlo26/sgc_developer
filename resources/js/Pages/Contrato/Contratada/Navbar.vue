55fe3dc4c4168d7968dbbf341a2339a6cedb2ba9
knancia <oliveira.augusto@outlook.com>
2024-03-22 03:44:54 -0300

    Tela inicial Contrato Serviços

--- resources/js/Pages/Contrato/Contratada/Navbar.vue
+++ resources/js/Pages/Contrato/Contratada/Navbar.vue
@@ -14,37 +14,39 @@ const props = defineProps({
 
 
 <template>
-  <div class="d-flex">
-    <div class="col-2">
-      <ul class="navbar-nav">
-        <!-- Dashboard -->
-        <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Dados Gerais"
-          :icon="IconLayoutDashboard" />
-
-        <!-- Contratos -->
-        <NavDropdown prefix="contratos.*" title="Recuros" :icon="IconLayoutDashboard">
-          <!-- Gestão Ambiental -->
-          <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="1"
-            active-on-route-prefix="contratos.gestao.*" title="RH" />
-
-          <!-- Estudo Ambiental -->
-          <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="2"
-            active-on-route-prefix="contratos.gestao.*" title="Equipamentos" />
-
-          <!-- Regularização Ambiental -->
-          <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="3"
-            active-on-route-prefix="contratos.gestao.*" title="Veículos" />
-        </NavDropdown>
-
-        <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Serviços"
-          :icon="IconLayoutDashboard" />
-
-        <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Cronograma"
-          :icon="IconLayoutDashboard" />
-      </ul>
-    </div>
-    <div class="col">
-      <slot name="body" />
+  <div class="card card-body space-y-3">
+    <div class="d-flex">
+      <div class="col">
+        <ul class="navbar-nav">
+          <!-- Dashboard -->
+          <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Dados Gerais"
+            :icon="IconLayoutDashboard" />
+  
+          <!-- Contratos -->
+          <NavDropdown prefix="contratos.*" title="Recuros" :icon="IconLayoutDashboard">
+            <!-- Gestão Ambiental -->
+            <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="1"
+              active-on-route-prefix="contratos.gestao.*" title="RH" />
+  
+            <!-- Estudo Ambiental -->
+            <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="2"
+              active-on-route-prefix="contratos.gestao.*" title="Equipamentos" />
+  
+            <!-- Regularização Ambiental -->
+            <NavDropdownLink route-name="contratos.gestao.listagem" param-name="tipo" route-param="3"
+              active-on-route-prefix="contratos.gestao.*" title="Veículos" />
+          </NavDropdown>
+  
+          <navLink route-name="contratos.contratada.servicos.index" :param="contrato.id" title="Serviços"
+            :icon="IconLayoutDashboard" />
+  
+          <navLink route-name="contratos.contratada.dados_gerais.index" :param="contrato.id" title="Cronograma"
+            :icon="IconLayoutDashboard" />
+        </ul>
+      </div>
+      <div class="col-11">
+        <slot name="body" />
+      </div>
     </div>
   </div>
 </template>
