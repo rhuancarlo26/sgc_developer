0deeb761f439e56da4cd5aa59f8c36cdb5d3afa3
knancia <oliveira.augusto@outlook.com>
2024-03-26 17:44:34 -0300

    front-end tela de cadastro de novos serviços

--- resources/js/Pages/Contrato/Contratada/Servicos/Index.vue
+++ resources/js/Pages/Contrato/Contratada/Servicos/Index.vue
@@ -2,7 +2,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import Breadcrumb from "@/Components/Breadcrumb.vue";
 import Navbar from "../Navbar.vue";
-import TabServicos from "./Servicos/TabServicos.vue";
+import ModalNovoServico from "./Modais/ModalNovoServico.vue";
 
 defineProps({
     contrato: Object,
@@ -31,7 +31,108 @@ defineProps({
 
         <Navbar :contrato="contrato">
             <template #body>
-                <TabServicos/>
+                <div class="w-100 d-flex justify-content-end mb-4">
+                    <div class="container-buttons">
+                        <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#modalNovoServico">
+                            Novo Serviço
+                        </button>
+                    </div>
+                </div>
+                <!-- Listagem-->
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Tema</th>
+                                <th>Serviço</th>
+                                <th>Especificação</th>
+                                <th>Licença</th>
+                                <th>Status Aprovação</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    1
+                                </td>
+                                <td>
+                                    Supervisão Ambiental
+                                </td>
+                                <td>
+                                    Controle de Ocorrências
+                                </td>
+                                <td>
+                                    teste
+                                </td>
+                                <td>
+                                    LI - 1336/2020 - N/A
+                                </td>
+                                <td>
+                                    <span class="badge text-bg-success">
+                                        Vigente
+                                    </span>
+                                </td>
+                                <td>
+                                    ...
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    2
+                                </td>
+                                <td>
+                                    Supervisão Ambiental
+                                </td>
+                                <td>
+                                    Controle de Ocorrências
+                                </td>
+                                <td>
+                                    teste
+                                </td>
+                                <td>
+                                    LI - 1336/2020 - N/A
+                                </td>
+                                <td>
+                                    <span class="badge text-bg-warning">
+                                        Em Análise
+                                    </span>
+                                </td>
+                                <td>
+                                    ...
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    3
+                                </td>
+                                <td>
+                                    Supervisão Ambiental
+                                </td>
+                                <td>
+                                    Controle de Ocorrências
+                                </td>
+                                <td>
+                                    teste
+                                </td>
+                                <td>
+                                    LI - 1336/2020 - N/A
+                                </td>
+                                <td>
+                                    <span class="badge text-bg-info">
+                                        Não Vigente
+                                    </span>
+                                </td>
+                                <td>
+                                    ...
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <ModalNovoServico />
             </template>
         </Navbar>
 
