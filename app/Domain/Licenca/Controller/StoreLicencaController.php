f872d9fec246af19110cb928103bedd4a1c8c172
lino <linox_2@hotmail.com>
2024-03-26 15:22:39 -0300

    Feat: Correção de erros - ECO-73

--- app/Domain/Licenca/Controller/StoreLicencaController.php
+++ app/Domain/Licenca/Controller/StoreLicencaController.php
@@ -4,6 +4,7 @@ namespace App\Domain\Licenca\Controller;
 
 use App\Domain\Licenca\Requests\StoreLicencaRequest;
 use App\Domain\Licenca\Services\LicencaService;
+use App\Models\Licenca;
 use App\Shared\Http\Controllers\Controller;
 
 class StoreLicencaController extends Controller
@@ -12,8 +13,14 @@ class StoreLicencaController extends Controller
     { }
 
     public function index(StoreLicencaRequest $request): \Illuminate\Http\RedirectResponse
-    {
-        $response = $this->listagemLicenca->create(post: $request->validated());
+    {   
+        
+        $post = [
+               'user_id' => auth()->user()->id,
+            ...$request->all()
+        ];
+
+        $response = $this->listagemLicenca->create(post: $post);
 
         return to_route(route: 'licenca.create', parameters: $response['licenca'])
             ->with('message', $response['request']);
