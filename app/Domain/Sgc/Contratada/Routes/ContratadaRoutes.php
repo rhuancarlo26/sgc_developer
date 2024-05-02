e618cccc5d7eaeaeacfa9fdb8926bc2cc253cb97
leocasttro <leocasttro01@icloud.com>
2024-05-02 15:12:31 -0300

    feature: template de rederizacao de word

--- app/Domain/Sgc/Contratada/Routes/ContratadaRoutes.php
+++ app/Domain/Sgc/Contratada/Routes/ContratadaRoutes.php
@@ -4,12 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 use App\Domain\Sgc\Contratada\app\Controller\ContratoSgcController;
 use App\Domain\Sgc\Contratada\app\Controller\DadosGeraisSgcController;
+use App\Domain\Sgc\Contratada\app\Controller\RecursoSgcController;
 use App\Domain\Contrato\Contratada\DadosGerais\Introducao\Controller\StoreIntroducaoContratadaController;
 
 
 Route::prefix('/contratada')->group(function () {
     Route::get('{contrato}/',                                      [ContratoSgcController::class,                           'index'])->name('sgc.contratada.index');
-    Route::get('{contrato}/dados_gerais',                          [DadosGeraisSgcController::class,                         'index'])->name('sgc.contratada.dados_gerais.index');
+    Route::get('{contrato}/dados_gerais',                          [DadosGeraisSgcController::class,                        'index'])->name('sgc.contratada.dados_gerais.index');
+    Route::get('{contrato}/recurso',                               [RecursoSgcController::class,                            'index'])->name('sgc.contratada.recurso.index');
     Route::post('/store_introducao',                               [StoreIntroducaoContratadaController::class,             'index'])->name('contratos.contratada.store_introducao.index');
-    
+
 });
