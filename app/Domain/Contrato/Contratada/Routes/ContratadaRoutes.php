e4ee34b587324d1b3e79185392b557d40ab29323
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-05-29 14:38:33 -0300

    feat: Novos campos - ECO-149

--- app/Domain/Contrato/Contratada/Routes/ContratadaRoutes.php
+++ app/Domain/Contrato/Contratada/Routes/ContratadaRoutes.php
@@ -27,6 +27,7 @@ use App\Domain\Contrato\Contratada\Recurso\Equipamento\Controller\DestroyEquipam
 use App\Domain\Contrato\Contratada\Recurso\Equipamento\Controller\ListagemEquipamentoRecursoController;
 use App\Domain\Contrato\Contratada\Recurso\Equipamento\Controller\StoreDocumentoEquipamentoRecursoController;
 use App\Domain\Contrato\Contratada\Recurso\Equipamento\Controller\StoreEquipamentoRecursoController;
+use App\Domain\Contrato\Contratada\Recurso\Equipamento\Controller\UpdateEquipamentoRecursoController;
 use App\Domain\Contrato\Contratada\Recurso\Equipamento\Controller\VisualizarDocumentoEquipamentoRecursoController;
 use App\Domain\Contrato\Contratada\Recurso\Rh\Controller\CreateRhRecursoController;
 use App\Domain\Contrato\Contratada\Recurso\Rh\Controller\DestroyDocumentoRhRecursoController;
@@ -81,6 +82,7 @@ Route::prefix('/contratada')->group(function () {
     Route::get('{contrato}/recurso/equipamento',                           [ListagemEquipamentoRecursoController::class,            'index'])->name('contratos.contratada.recurso.equipamento.index');
     Route::get('{contrato}/recurso/equipamento/create/{equipamento?}',     [CreateEquipamentoRecursoController::class,              'index'])->name('contratos.contratada.recurso.equipamento.create');
     Route::post('recurso/equipamento/store',                               [StoreEquipamentoRecursoController::class,               'index'])->name('contratos.contratada.recurso.equipamento.store');
+    Route::patch('recurso/equipamento/update',                             [UpdateEquipamentoRecursoController::class,               'index'])->name('contratos.contratada.recurso.equipamento.update');
     Route::post('recurso/equipamento/store_documento',                     [StoreDocumentoEquipamentoRecursoController::class,      'index'])->name('contratos.contratada.recurso.equipamento.store_documento');
     Route::get('recurso/equipamento/visualizar/{documento}',               [VisualizarDocumentoEquipamentoRecursoController::class, 'index'])->name('contratos.contratada.recurso.equipamento.visualizar');
     Route::delete('{contrato}/recurso/equipamento/destroy/{documento}',    [DestroyDocumentoEquipamentoRecursoController::class,    'index'])->name('contratos.contratada.recurso.equipamento.destroy');
