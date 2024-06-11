79efc6d57d5892966098163e4bb085af15face87
Jeferson Marques <joedf1006@gmail.com>
2024-06-11 16:16:42 -0300

    :recycle: refactor: add user_id as migrations de documentos e documentos baixa, refaotrar documentos e documentos baixa.

--- app/Domain/Contrato/Contratada/Recurso/Rh/Controller/StoreDocumentoBaixaRhRecursoController.php
+++ app/Domain/Contrato/Contratada/Recurso/Rh/Controller/StoreDocumentoBaixaRhRecursoController.php
@@ -2,9 +2,7 @@
 
 namespace App\Domain\Contrato\Contratada\Recurso\Rh\Controller;
 
-use App\Domain\Contrato\Contratada\Recurso\Equipamento\Services\EquipamentoRecursoService;
 use App\Domain\Contrato\Contratada\Recurso\Rh\Services\RhRecursoService;
-use App\Domain\Contrato\Contratada\Recurso\Veiculo\Services\VeiculoRecursoService;
 use App\Shared\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -18,6 +16,9 @@ class StoreDocumentoBaixaRhRecursoController extends Controller
   {
     $response = $this->rhRecursoService->salvarDocumentoBaixaRh($request->all());
 
-    return to_route('contratos.contratada.recurso.rh.create', ['contrato' => $request->contrato_id, 'rh' => $request->recurso_rh_id])->with('message', $response);
+    return back()->withInput([
+      'contrato' => $request->contrato_id,
+      'rh' => $response['rh']
+    ])->with('message', $response['request']);
   }
 }
