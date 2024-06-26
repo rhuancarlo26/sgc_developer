cd7d13a095c7c649cf6fd5a4799010ed3ebbc356
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-06-26 15:59:50 -0300

    feat: CRUD campanha - ECO-163

--- app/Domain/Servico/PMQA/Execucao/Routes/ExecucaoRoutes.php
+++ app/Domain/Servico/PMQA/Execucao/Routes/ExecucaoRoutes.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Domain\Servico\PMQA\Execucao\Controller\DestroyController;
 use App\Domain\Servico\PMQA\Execucao\Controller\IndexController;
+use App\Domain\Servico\PMQA\Execucao\Controller\StoreController;
+use App\Domain\Servico\PMQA\Execucao\Controller\UpdateController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/execucao')->group(function () {
-  Route::get('{contrato}/{servico}/',                   [IndexController::class, 'index'])->name('contratos.contratada.servicos.pmqa.execucao.index');
+  Route::get('{contrato}/{servico}/',                       [IndexController::class, 'index'])->name('contratos.contratada.servicos.pmqa.execucao.index');
+  Route::post('{contrato}/{servico}/store',                 [StoreController::class, 'index'])->name('contratos.contratada.servicos.pmqa.execucao.store');
+  Route::patch('{contrato}/{servico}/update',               [UpdateController::class, 'index'])->name('contratos.contratada.servicos.pmqa.execucao.update');
+  Route::delete('{contrato}/{servico}/destroy/{campanha}',  [DestroyController::class, 'index'])->name('contratos.contratada.servicos.pmqa.execucao.destroy');
 });
