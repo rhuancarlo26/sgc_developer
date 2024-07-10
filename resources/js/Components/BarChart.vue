649390337099db17efbafcf09dc4b16ffbd6dd65
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-07-10 02:07:48 -0300

    feat: pequenos ajustes - ECO-170

---
+++ resources/js/Components/BarChart.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { ref } from 'vue'
+
+const props = defineProps({
+  chart_data: { type: Object, default: {} },
+  chart_options: { type: Object, default: {} }
+});
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+</script>
+<template>
+  <Bar id="my-chart-id" :options="chart_options" :data="chart_data" />
+</template>
--- resources/js/Pages/Servico/PMQA/Resultado/Resultado.vue
+++ resources/js/Pages/Servico/PMQA/Resultado/Resultado.vue
@@ -2,7 +2,9 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import Breadcrumb from "@/Components/Breadcrumb.vue";
 import Navbar from "../Navbar.vue";
-import { Head, Link } from "@inertiajs/vue3";
+import InputLabel from "@/Components/InputLabel.vue";
+import InputError from "@/Components/InputError.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
 import ModelSearchFormAllColumns from "@/Components/ModelSearchFormAllColumns.vue";
 import Table from "@/Components/Table.vue";
 import NavButton from "@/Components/NavButton.vue";
@@ -15,12 +17,27 @@ import { IconPencil } from "@tabler/icons-vue";
 import { IconSettings } from "@tabler/icons-vue";
 import ModalResultado from "./ModalResultado.vue";
 import { computed } from "vue";
+import BarChart from "@/Components/BarChart.vue";
+import { IconDeviceFloppy } from "@tabler/icons-vue";
 
 const props = defineProps({
   contrato: { type: Object },
   servico: { type: Object },
   resultado: { type: Object },
   parametros: { type: Array }
+});
+
+const form = useForm({
+  resultado_id: props.resultado.id,
+  analises: []
+});
+
+onMounted(() => {
+  if (props.resultado.analises.length) {
+    props.resultado.analises.forEach(analise => {
+      form.analises[analise.parametro_id] = analise.analise;
+    });
+  }
 });
 
 const filtroParametros = computed(() => {
@@ -34,16 +51,21 @@ const filtroParametros = computed(() => {
   props.resultado.campanhas.forEach(campanha => {
     campanha.pontos.forEach(ponto => {
       ponto.lista.parametros_vinculados.forEach(vinculado => {
-        uniqueParametros[vinculado.parametro_id][vinculado.lista_parametro_id] = {
-          nome_lista: ponto.lista.nome,
-          medicao: vinculado.medicao.medicao
-        }
+
       });
     });
   });
-
+  console.log('parametros', uniqueParametros);
   return uniqueParametros;
 });
+
+const salvarAnalise = () => {
+  if (props.resultado.analises.length) {
+    form.patch(route('contratos.contratada.servicos.pmqa.resultado.update_analise', { contrato: props.contrato.id, servico: props.servico.id, resultado: props.resultado.id }))
+  } else {
+    form.post(route('contratos.contratada.servicos.pmqa.resultado.store_analise', { contrato: props.contrato.id, servico: props.servico.id, resultado: props.resultado.id }))
+  }
+}
 
 </script>
 <template>
@@ -73,7 +95,16 @@ const filtroParametros = computed(() => {
             <li v-for="parametro, key, index in filtroParametros" :key="parametro.id" class="nav-item"
               role="presentation">
               <a :href="'#tabs-parametro-' + parametro.id" class="nav-link" :class="index === 0 ? 'active' : ''"
-                data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">{{ parametro.nome }}</a>
+                data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">{{ `${parametro.nome}
+                ${parametro.unidade ? ` - ${parametro.unidade}` : ''}` }}</a>
+            </li>
+            <li class="nav-item" role="presentation">
+              <a href="#tabs-parametro-iqa" class="nav-link" data-bs-toggle="tab" aria-selected="false" tabindex="-1"
+                role="tab">IQA</a>
+            </li>
+            <li class="nav-item" role="presentation">
+              <a href="#tabs-parametro-outra-analise" class="nav-link" data-bs-toggle="tab" aria-selected="false"
+                tabindex="-1" role="tab">Outras analises</a>
             </li>
           </ul>
         </div>
@@ -81,8 +112,52 @@ const filtroParametros = computed(() => {
           <div class="tab-content">
             <div v-for="parametro, key, index in filtroParametros" :key="parametro.id" class="tab-pane"
               :class="index === 0 ? 'active show' : ''" :id="'tabs-parametro-' + parametro.id" role="tabpanel">
-              <h4>{{ parametro.nome }}</h4>
-              <pre>{{ parametro }}</pre>
+              <BarChart :chart_data="{
+                labels: ['Medição'],
+                datasets: [{
+                  label: 'Data One',
+                  backgroundColor: '#f87979',
+                  data: [2]
+                }, {
+                  label: 'Data One',
+                  backgroundColor: '#f87900',
+                  data: [1]
+                }]
+              }" :chart_options="{ responsive: true }"></BarChart>
+              <div>
+                <div class="row mb-4">
+                  <div class="col form-group">
+                    <InputLabel value="Analise" for="analise" />
+                    <textarea :name="'analise-' + parametro.id" :id="'analise-' + parametro.id" class="form-control"
+                      rows="6" v-model="form.analises[parametro.id]"></textarea>
+                    <InputError :message="form.errors.analise" />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col form-group d-flex justify-content-end">
+                    <NavButton @click="salvarAnalise()" type-button="success" :icon="IconDeviceFloppy" title="Salvar" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="tab-pane" id="tabs-parametro-iqa" role="tabpanel">
+              <div>
+                <div class="row mb-4">
+                  <div class="col form-group">
+                    <InputLabel value="Analise" for="analise" />
+                    <textarea name="analise-iqa" id="analise-iqa" class="form-control" rows="6"></textarea>
+                    <InputError :message="form.errors.analise" />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col form-group d-flex justify-content-end">
+                    <NavButton @click="salvarAnalise()" type-button="success" :icon="IconDeviceFloppy" title="Salvar" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="tab-pane" id="tabs-parametro-outra-analise" role="tabpanel">
+              Outra analise
             </div>
           </div>
         </div>
