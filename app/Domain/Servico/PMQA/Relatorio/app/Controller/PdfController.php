3db359fd5cc10dd27c5b6d7ece0759349535086f
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-07-24 04:10:23 -0300

    feat: pequenos ajustes - ECO1-041

--- app/Domain/Servico/PMQA/Relatorio/app/Controller/PdfController.php
+++ app/Domain/Servico/PMQA/Relatorio/app/Controller/PdfController.php
@@ -10,6 +10,7 @@ use App\Models\Servicos;
 use App\Shared\Http\Controllers\Controller;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -31,10 +32,34 @@ class PdfController extends Controller
 
     $pontosVinculados = collect($relatorio->resultado->campanhas)->pluck('pontos')->flatten()->unique('id')->values();
     $parametrosVinculados = collect($relatorio->resultado->campanhas)->pluck('pontos')->flatten()->pluck('lista.parametros')->flatten()->unique('id')->values();
-    $campanhaPontos = collect($relatorio->resultado->campanhas)->pluck('pontos')->flatten()->pluck('nomepontocoleta')->unique()->values();
+    $analiseIqa = $this->showImageAsBase64($relatorio->resultado->analise_iqa->caminho);
+    $analises = $relatorio->resultado->analises->mapWithKeys(function ($item) {
+      $item['imagem'] = $this->showImageAsBase64($item->caminho);
 
-    $pdf = Pdf::loadView('Servico.Pmqa.Relatorio.RelatorioPdf', compact('contrato', 'servico', 'relatorio', 'pontosVinculados', 'parametrosVinculados', 'campanhaPontos'));
+      return [$item['parametro_id'] => $item];
+    });
+    $outrasAnalises = $relatorio->resultado->outras_analises->map(function ($item) {
+      $item['imagem'] = $this->showImageAsBase64($item->caminho);
+
+      return $item;
+    });
+
+    $pdf = Pdf::loadView('Servico.Pmqa.Relatorio.RelatorioPdf', compact('contrato', 'servico', 'relatorio', 'pontosVinculados', 'parametrosVinculados', 'analiseIqa', 'analises', 'outrasAnalises'));
 
     return $pdf->stream();
+  }
+
+  public function showImageAsBase64($caminho)
+  {
+    if (Storage::disk('public')->exists($caminho)) {
+      $imageContents = Storage::disk('public')->get($caminho);
+      $mimeType = Storage::disk('public')->mimeType($caminho);
+      $base64Image = base64_encode($imageContents);
+      $base64ImageUrl = 'data:' . $mimeType . ';base64,' . $base64Image;
+
+      return $base64ImageUrl;
+    } else {
+      return null;
+    }
   }
 }
