b58fd161314aa90cbab1e61bcc7f513ed192cd89
mcamatta <camattamichel@gmail.com>
2024-07-30 11:21:25 -0300

    feat: adiciona delete de plano e exibicao de pdf

--- app/Domain/Servico/SupressaoVegetacao/Configuracao/PlanoSupressao/Routes/PlanoSupressaoRoutes.php
+++ app/Domain/Servico/SupressaoVegetacao/Configuracao/PlanoSupressao/Routes/PlanoSupressaoRoutes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Domain\Servico\SupressaoVegetacao\Configuracao\PlanoSupressao\Controller\DeleteController;
 use App\Domain\Servico\SupressaoVegetacao\Configuracao\PlanoSupressao\Controller\IndexController;
 use App\Domain\Servico\SupressaoVegetacao\Configuracao\PlanoSupressao\Controller\StoreController;
 use Illuminate\Support\Facades\Route;
@@ -7,4 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/plano-supressao')->group(function () {
     Route::get('/{contrato}/{servico}', IndexController::class)->name('contratos.contratada.servicos.supressao-vegetacao.configuracao.plano-supressao.index');
     Route::post('/store',               StoreController::class)->name('contratos.contratada.servicos.supressao-vegetacao.configuracao.plano-supressao.store');
+    Route::delete('/{plano}',           DeleteController::class)->name('contratos.contratada.servicos.supressao-vegetacao.configuracao.plano-supressao.destroy');
 });
--- app/Models/Arquivo.php
+++ app/Models/Arquivo.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,4 +12,13 @@ class Arquivo extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = ['id', 'created_at'];
+
+    protected $appends = ['caminho'];
+
+    public function caminho(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => asset('storage/' . $this->diretorio . $this->arquivo)
+        );
+    }
 }
