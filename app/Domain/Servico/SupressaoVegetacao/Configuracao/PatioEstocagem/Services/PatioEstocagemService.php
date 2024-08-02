5aafa72032e197180eb23cc5c0406c5c1fc5d114
mcamatta <camattamichel@gmail.com>
2024-08-02 09:10:02 -0300

    feat: adiciona delete de fotos e de patio

--- app/Domain/Servico/SupressaoVegetacao/Configuracao/PatioEstocagem/Services/PatioEstocagemService.php
+++ app/Domain/Servico/SupressaoVegetacao/Configuracao/PatioEstocagem/Services/PatioEstocagemService.php
@@ -3,6 +3,7 @@
 namespace App\Domain\Servico\SupressaoVegetacao\Configuracao\PatioEstocagem\Services;
 
 use App\Domain\Licenca\Shapefile\Services\LicencaShapefileService;
+use App\Models\Arquivo;
 use App\Models\PatioEstocagem;
 use App\Models\Servicos;
 use App\Shared\Abstract\BaseModelService;
@@ -38,40 +39,56 @@ class PatioEstocagemService extends BaseModelService
 
     public function store(array $request): array
     {
-        $shapefile = $request['shapefile'];
-        if ($shapefile) {
-            $shape = $this->licencaShapefileService->getFeatureCollection(file: $shapefile);
-            $request['shapefile'] = $shape;
-            $path = storage_path('app' . DIRECTORY_SEPARATOR . 'file_shape' . DIRECTORY_SEPARATOR . uniqid() . '.json');
-            file_put_contents($path, $shape);
-            $request['local_shape'] = $path;
-        }
-
+        $this->handleShapefile(request: $request);
         $response = $this->dataManagement->create(entity: $this->modelClass, infos: [
             ...$request,
             'chave' => $this->getCodigo(),
         ]);
-
-        if (isset($request['fotos']) && count($request['fotos'])) {
-            /** @var UploadedFile[] $fotos */
-            $fotos = $request['fotos'];
-            $fotosId = array_map(function($foto) {
-                $arquivo = $this->arquivoUtils->salvar(arquivo: $foto, diretorio: 'public/uploads/supressao/patio/', prefixo: 'PT');
-                return $arquivo?->id;
-            }, $fotos);
-
-            $response['model']?->fotos()->sync($fotosId);
-        }
-
+        $this->handleFotos(request: $request, afterSave: fn(array $fotosId) => $response['model']?->fotos()->sync($fotosId));
         return $response;
     }
 
-//    public function update(array $request): array
-//    {
-//        $response = $this->dataManagement->update(entity: $this->modelClass, infos: $request, id: $request['id']);
-//
-//        return $response;
-//    }
+    public function update(array $request): array
+    {
+        $this->handleShapefile(request: $request);
+        $response = $this->dataManagement->update(entity: $this->modelClass, infos: $request, id: $request['id']);
+        $this->handleFotos(request: $request, afterSave: fn(array $fotosId) => $this->model->find($request['id'])?->fotos()->attach($fotosId));
+        return $response;
+    }
+
+    public function deleteFoto(Arquivo $arquivo, PatioEstocagem $patio): bool
+    {
+        if($this->arquivoUtils->delete(arquivo: $arquivo)) {
+            $patio->fotos()->detach($arquivo->id);
+            return true;
+        }
+        return false;
+    }
+
+    private function handleFotos(array &$request, callable $afterSave): void
+    {
+        if (!isset($request['fotos']) || !count($request['fotos'])) {
+            return;
+        }
+        /** @var UploadedFile[] $fotos */
+        $fotos = $request['fotos'];
+        $fotosId = array_map(function($foto) {
+            $arquivo = $this->arquivoUtils->salvar(arquivo: $foto, diretorio: 'public/uploads/supressao/patio/', prefixo: 'PT');
+            return $arquivo?->id;
+        }, $fotos);
+        $afterSave($fotosId);
+    }
+
+    private function handleShapefile(array &$request): void
+    {
+        $shapefile = $request['shapefile'];
+        if (!($shapefile instanceof UploadedFile)) return;
+        $shape = $this->licencaShapefileService->getFeatureCollection(file: $shapefile);
+        $request['shapefile'] = $shape;
+        $path = storage_path('app' . DIRECTORY_SEPARATOR . 'file_shape' . DIRECTORY_SEPARATOR . uniqid() . '.json');
+        file_put_contents($path, $shape);
+        $request['local_shape'] = $path;
+    }
 
     private function getCodigo(): string
     {
