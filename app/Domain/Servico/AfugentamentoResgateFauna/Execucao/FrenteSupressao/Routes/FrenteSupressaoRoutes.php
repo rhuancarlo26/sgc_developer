3270595d6a1f05cc4fbe2dfa2bbe9853d38d0c1e
Jeferson Marques <joedf1006@gmail.com>
2024-08-16 18:54:17 -0300

    :sparkles: feat: [Execução] -> frente supressão e registros

--- app/Domain/Servico/AfugentamentoResgateFauna/Execucao/FrenteSupressao/Routes/FrenteSupressaoRoutes.php
+++ app/Domain/Servico/AfugentamentoResgateFauna/Execucao/FrenteSupressao/Routes/FrenteSupressaoRoutes.php
@@ -1,9 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Domain\Servico\AfugentamentoResgateFauna\Execucao\FrenteSupressao\Controller\CreateFrenteSupressaoController;
+use App\Domain\Servico\AfugentamentoResgateFauna\Execucao\FrenteSupressao\Controller\DestroyFrenteSupressaoController;
 use App\Domain\Servico\AfugentamentoResgateFauna\Execucao\FrenteSupressao\Controller\FrenteSupressaoController;
+use App\Domain\Servico\AfugentamentoResgateFauna\Execucao\FrenteSupressao\Controller\UpdateFrenteSupressaoController;
+use Illuminate\Support\Facades\Route;
 
 Route::prefix('/FrenteSupressao')->group(function () {
-    Route::get('{contrato}/{servico}/',           [FrenteSupressaoController::class,      'index'])->name('contratos.contratada.servicos.afugentamento.resgate.fauna.execucao.frente.supressao.index');
-    Route::post('/',           [FrenteSupressaoController::class,      'index'])->name('contratos.contratada.servicos.afugentamento.resgate.fauna.execucao.frente.supressao.create');
+    Route::get('{contrato}/{servico}/',     [FrenteSupressaoController::class,            'index'])->name('contratos.contratada.servicos.afugentamento.resgate.fauna.execucao.frente.supressao.index');
+    Route::post('/{servico}',               [CreateFrenteSupressaoController::class,      'index'])->name('contratos.contratada.servicos.afugentamento.resgate.fauna.execucao.frente.supressao.create');
+    Route::delete('/{frente}',              [DestroyFrenteSupressaoController::class,     'index'])->name('contratos.contratada.servicos.afugentamento.resgate.fauna.execucao.frente.supressao.delete');
+    Route::patch('/{frente}',               [UpdateFrenteSupressaoController::class,      'index'])->name('contratos.contratada.servicos.afugentamento.resgate.fauna.execucao.frente.supressao.update');
 });
