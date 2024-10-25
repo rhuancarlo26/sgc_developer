8b197b55dfd64818abef9e1ac699c93836c01102
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-10-25 14:50:23 -0300

    feat: Enivo fiscal - ECO-074

--- resources/js/Pages/Servico/PassagemFauna/Configuracao/PassagemFauna/Index.vue
+++ resources/js/Pages/Servico/PassagemFauna/Configuracao/PassagemFauna/Index.vue
@@ -2,14 +2,14 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import Breadcrumb from "@/Components/Breadcrumb.vue";
 import Navbar from "../../Navbar.vue";
-import {Head, Link, router} from "@inertiajs/vue3";
+import { Head, Link, router } from "@inertiajs/vue3";
 import ModelSearchFormAllColumns from "@/Components/ModelSearchFormAllColumns.vue";
 import Table from "@/Components/Table.vue";
 import NavButton from "@/Components/NavButton.vue";
-import {dateTimeFormat} from "@/Utils/DateTimeUtils.js";
+import { dateTimeFormat } from "@/Utils/DateTimeUtils.js";
 import ModalImportarPassagem from "./ModalImportarPassagem.vue";
-import {ref} from "vue";
-import {IconEye, IconPencil, IconTrash} from "@tabler/icons-vue";
+import { ref } from "vue";
+import { IconEye, IconPencil, IconTrash } from "@tabler/icons-vue";
 import ModalVisualizarPassagemFauna from "./ModalVisualizarPassagemFauna.vue";
 import LinkConfirmation from "@/Components/LinkConfirmation.vue";
 import ModalForm from "./ModalForm.vue";
@@ -18,9 +18,9 @@ const modalImportarPassagem = ref({});
 const modalVisualizarPassagemFauna = ref({});
 const modalForm = ref({});
 const props = defineProps({
-    contrato: {type: Object},
-    servico: {type: Object},
-    passagens: {type: Object}
+    contrato: { type: Object },
+    servico: { type: Object },
+    passagens: { type: Object }
 });
 
 const abrirModalImportarPassagem = () => {
@@ -45,20 +45,20 @@ const submeterFiscal = () => {
 </script>
 <template>
 
-    <Head :title="`${contrato.contratada.slice(0, 10)}...`"/>
+    <Head :title="`${contrato.contratada.slice(0, 10)}...`" />
 
     <AuthenticatedLayout>
 
         <template #header>
             <div class="w-100 d-flex justify-content-between">
                 <Breadcrumb class="align-self-center" :links="[
-          { route: route('contratos.gestao.listagem', contrato.tipo_contrato), label: `Gestão de Contratos` },
-          { route: '#', label: contrato.contratada }
-        ]
-          "/>
+                    { route: route('contratos.gestao.listagem', contrato.tipo_contrato), label: `Gestão de Contratos` },
+                    { route: '#', label: contrato.contratada }
+                ]
+                    " />
                 <Link class="btn btn-dark"
-                      :href="route('contratos.contratada.servicos.index', { contrato: props.contrato.id })">
-                    Voltar
+                    :href="route('contratos.contratada.servicos.index', { contrato: props.contrato.id })">
+                Voltar
                 </Link>
             </div>
         </template>
@@ -66,14 +66,13 @@ const submeterFiscal = () => {
         <Navbar :contrato="contrato" :servico="servico">
             <template #body>
                 <ModelSearchFormAllColumns :columns="[]">
-                    <template #action>
-                        <NavButton v-if="!servico.parecer_passagem_fauna" @click="submeterFiscal()"
-                                   type-button="primary" title="Submeter ao fiscal"/>
+                    <template #action
+                        v-if="!servico.parecer_passagem_fauna || ![1, 3].includes(servico.parecer_passagem_fauna.fk_status)">
+                        <NavButton @click="submeterFiscal()" type-button="primary" title="Submeter ao fiscal" />
                         <a class="btn btn-info me-1" target="_blank"
-                           :href="route('contratos.contratada.servicos.passagem_fauna.configuracao.passagem_fauna.download_modelo', {contrato: contrato.id, servico: servico.id})">Modelo</a>
-                        <NavButton @click="abrirModalImportarPassagem()" class="nav-item"
-                                   type-button="success" title="Importar"
-                        />
+                            :href="route('contratos.contratada.servicos.passagem_fauna.configuracao.passagem_fauna.download_modelo', { contrato: contrato.id, servico: servico.id })">Modelo</a>
+                        <NavButton @click="abrirModalImportarPassagem()" class="nav-item" type-button="success"
+                            title="Importar" />
                     </template>
                 </ModelSearchFormAllColumns>
                 <Table
@@ -93,19 +92,20 @@ const submeterFiscal = () => {
                             <td>{{ dateTimeFormat(item.created_at) }}</td>
                             <td>
                                 <NavButton @click="abrirModalVisualizarPassagemFauna(item)" :icon="IconEye"
-                                           class="btn-icon"
-                                           type-button="info"/>
-                                <NavButton @click="abrirModalForm(item)" :icon="IconPencil"
-                                           class="btn-icon"
-                                           type-button="primary"/>
-                                <LinkConfirmation v-slot="confirmation"
-                                                  :options="{ text: 'A remoção da passagem de fauna será permanente.' }">
-                                    <Link :onBefore="confirmation.show"
-                                          :href="route('contratos.contratada.servicos.passagem_fauna.configuracao.passagem_fauna.delete', { contrato: contrato.id, servico: servico.id, passagem_fauna: item.id })"
-                                          as="button" method="delete" type="button" class="btn btn-icon btn-danger">
-                                        <IconTrash/>
-                                    </Link>
-                                </LinkConfirmation>
+                                    class="btn-icon" type-button="info" />
+                                <span
+                                    v-if="!servico.parecer_passagem_fauna || ![1, 3].includes(servico.parecer_passagem_fauna.fk_status)">
+                                    <NavButton @click="abrirModalForm(item)" :icon="IconPencil" class="btn-icon"
+                                        type-button="primary" />
+                                    <LinkConfirmation v-slot="confirmation"
+                                        :options="{ text: 'A remoção da passagem de fauna será permanente.' }">
+                                        <Link :onBefore="confirmation.show"
+                                            :href="route('contratos.contratada.servicos.passagem_fauna.configuracao.passagem_fauna.delete', { contrato: contrato.id, servico: servico.id, passagem_fauna: item.id })"
+                                            as="button" method="delete" type="button" class="btn btn-icon btn-danger">
+                                        <IconTrash />
+                                        </Link>
+                                    </LinkConfirmation>
+                                </span>
                             </td>
                         </tr>
                     </template>
@@ -113,9 +113,9 @@ const submeterFiscal = () => {
             </template>
         </Navbar>
 
-        <ModalImportarPassagem :contrato="contrato" :servico="servico" ref="modalImportarPassagem"/>
-        <ModalVisualizarPassagemFauna ref="modalVisualizarPassagemFauna"/>
-        <ModalForm :contrato="contrato" :servico="servico" ref="modalForm"/>
+        <ModalImportarPassagem :contrato="contrato" :servico="servico" ref="modalImportarPassagem" />
+        <ModalVisualizarPassagemFauna ref="modalVisualizarPassagemFauna" />
+        <ModalForm :contrato="contrato" :servico="servico" ref="modalForm" />
 
     </AuthenticatedLayout>
 </template>
--- resources/js/Pages/Servico/PassagemFauna/Navbar.vue
+++ resources/js/Pages/Servico/PassagemFauna/Navbar.vue
@@ -2,13 +2,13 @@
 import NavDropdownLink from '@/Components/NavDropdownLink.vue';
 import NavDropdown from '@/Components/NavDropdown.vue';
 import Navbar from '@/Pages/Contrato/Contratada/Navbar.vue';
-import {IconLayoutDashboard} from '@tabler/icons-vue';
+import { IconLayoutDashboard } from '@tabler/icons-vue';
 import Breadcrumb from '@/Components/Breadcrumb.vue';
 import NavLink from '@/Components/NavLink.vue';
 
 const porps = defineProps({
-    contrato: {type: Object},
-    servico: {type: Object}
+    contrato: { type: Object },
+    servico: { type: Object }
 });
 </script>
 <template>
@@ -16,7 +16,7 @@ const porps = defineProps({
         <template #body>
             <div class="mb-4">
                 <Breadcrumb
-                    :links="[{ route: route('contratos.contratada.servicos.index', { contrato: contrato.id, servico: servico.id }), label: 'Serviços' }, { route: '#', label: servico?.tipo?.nome }]"/>
+                    :links="[{ route: route('contratos.contratada.servicos.index', { contrato: contrato.id, servico: servico.id }), label: 'Serviços' }, { route: '#', label: servico?.tipo?.nome }]" />
             </div>
             <div class="card card-body p-0 space-y-3">
                 <header class="navbar-expand-md">
@@ -33,16 +33,15 @@ const porps = defineProps({
                                                     route-name="contratos.contratada.servicos.passagem_fauna.configuracao.vincular_abio.index"
                                                     active-on-route-prefix="contratos.contratada.servicos.passagem_fauna.configuracao.vincular_abio*"
                                                     :route-param="{ contrato: contrato.id, servico: servico.id }"
-                                                    title="Vincular ABIO"/>
+                                                    title="Vincular ABIO" />
 
                                                 <NavDropdownLink
                                                     route-name="contratos.contratada.servicos.passagem_fauna.configuracao.passagem_fauna.index"
                                                     active-on-route-prefix="contratos.contratada.servicos.passagem_fauna.configuracao.passagem_fauna*"
                                                     :route-param="{ contrato: contrato.id, servico: servico.id }"
-                                                    title="Passagem fauna"/>
+                                                    title="Passagem fauna" />
                                             </NavDropdown>
-                                            <template
-                                                v-if="servico.parecer_passagem_fauna || servico.parecer_passagem_fauna?.fk_status === 3">
+                                            <template v-if="servico.parecer_passagem_fauna?.fk_status === 3">
                                                 <NavDropdown
                                                     prefix="contratos.contratada.servicos.passagem_fauna.execucao*"
                                                     title="Execução" :icon="IconLayoutDashboard">
@@ -50,27 +49,25 @@ const porps = defineProps({
                                                         route-name="contratos.contratada.servicos.passagem_fauna.execucao.campanha.index"
                                                         active-on-route-prefix="contratos.contratada.servicos.passagem_fauna.execucao.campanha*"
                                                         :route-param="{ contrato: contrato.id, servico: servico.id }"
-                                                        title="Campanha"/>
+                                                        title="Campanha" />
                                                     <NavDropdownLink
                                                         route-name="contratos.contratada.servicos.passagem_fauna.execucao.registro.index"
                                                         active-on-route-prefix="contratos.contratada.servicos.passagem_fauna.execucao.registro*"
                                                         :route-param="{ contrato: contrato.id, servico: servico.id }"
-                                                        title="Registros"/>
+                                                        title="Registros" />
                                                 </NavDropdown>
 
                                                 <NavLink
                                                     route-name="contratos.contratada.servicos.passagem_fauna.resultado.index"
                                                     active-on-route-prefix="contratos.contratada.servicos.passagem_fauna.resultado*"
                                                     :param="{ contrato: contrato.id, servico: servico.id }"
-                                                    title="Resultado"
-                                                    :icon="IconLayoutDashboard"/>
+                                                    title="Resultado" :icon="IconLayoutDashboard" />
 
                                                 <NavLink
                                                     route-name="contratos.contratada.servicos.passagem_fauna.relatorio.index"
                                                     active-on-route-prefix="contratos.contratada.servicos.passagem_fauna.relatorio*"
                                                     :param="{ contrato: contrato.id, servico: servico.id }"
-                                                    title="Relatório"
-                                                    :icon="IconLayoutDashboard"/>
+                                                    title="Relatório" :icon="IconLayoutDashboard" />
                                             </template>
                                         </ul>
                                     </div>
@@ -80,7 +77,7 @@ const porps = defineProps({
                     </div>
                 </header>
                 <div class="mt-2 card card-body">
-                    <slot name="body"/>
+                    <slot name="body" />
                 </div>
             </div>
         </template>
