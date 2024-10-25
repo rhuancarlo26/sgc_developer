3800a4d7605eece03bea9b2061112e47fbb6bc82
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-10-25 14:50:23 -0300

    feat: Enivo fiscal - ECO-074

--- resources/js/Pages/Servico/PassagemFauna/Configuracao/VincularABIO/Index.vue
+++ resources/js/Pages/Servico/PassagemFauna/Configuracao/VincularABIO/Index.vue
@@ -2,22 +2,22 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import Breadcrumb from "@/Components/Breadcrumb.vue";
 import Navbar from "../../Navbar.vue";
-import {Head, Link, router} from "@inertiajs/vue3";
+import { Head, Link, router } from "@inertiajs/vue3";
 import ModelSearchFormAllColumns from "@/Components/ModelSearchFormAllColumns.vue";
 import Table from "@/Components/Table.vue";
-import {dateTimeFormat} from "../../../../../Utils/DateTimeUtils.js";
-import {IconDots, IconPlus} from "@tabler/icons-vue";
+import { dateTimeFormat } from "../../../../../Utils/DateTimeUtils.js";
+import { IconDots, IconPlus } from "@tabler/icons-vue";
 import NavButton from "@/Components/NavButton.vue";
 import ModalVincularABIO from "./ModalVincularABIO.vue";
-import {ref} from "vue";
+import { ref } from "vue";
 import ModalAdicionarRET from "./ModalAdicionarRET.vue";
 
 const modalVincularABIO = ref({});
 const modalAdicionarRET = ref({});
 const props = defineProps({
-    contrato: {type: Object},
-    servico: {type: Object},
-    abios: {type: Object}
+    contrato: { type: Object },
+    servico: { type: Object },
+    abios: { type: Object }
 });
 
 const abrirModalVincularABIO = () => {
@@ -39,20 +39,20 @@ const excluirABIO = (abio_id) => {
 </script>
 <template>
 
-    <Head :title="`${contrato.contratada.slice(0, 10)}...`"/>
+    <Head :title="`${contrato.contratada.slice(0, 10)}...`" />
 
     <AuthenticatedLayout>
 
         <template #header>
             <div class="w-100 d-flex justify-content-between">
                 <Breadcrumb class="align-self-center" :links="[
-          { route: route('contratos.gestao.listagem', contrato.tipo_contrato), label: `Gestão de Contratos` },
-          { route: '#', label: contrato.contratada }
-        ]
-          "/>
+                    { route: route('contratos.gestao.listagem', contrato.tipo_contrato), label: `Gestão de Contratos` },
+                    { route: '#', label: contrato.contratada }
+                ]
+                    " />
                 <Link class="btn btn-dark"
-                      :href="route('contratos.contratada.servicos.index', { contrato: props.contrato.id })">
-                    Voltar
+                    :href="route('contratos.contratada.servicos.index', { contrato: props.contrato.id })">
+                Voltar
                 </Link>
             </div>
         </template>
@@ -60,10 +60,10 @@ const excluirABIO = (abio_id) => {
         <Navbar :contrato="contrato" :servico="servico">
             <template #body>
                 <ModelSearchFormAllColumns :columns="[]">
-                    <template #action>
+                    <template #action
+                        v-if="!servico.parecer_passagem_fauna || ![1, 3].includes(servico.parecer_passagem_fauna.fk_status)">
                         <NavButton @click="abrirModalVincularABIO()" :icon="IconPlus" class="nav-item"
-                                   type-button="success" title="Vincular ABIO"
-                        />
+                            type-button="success" title="Vincular ABIO" />
                     </template>
                 </ModelSearchFormAllColumns>
                 <Table
@@ -81,19 +81,22 @@ const excluirABIO = (abio_id) => {
                             <td>{{ item.licenca?.processo_dnit }}</td>
                             <td class="text-center">
                                 <button type="button" class="btn btn-icon btn-info dropdown-toggle p-2"
-                                        data-bs-boundary="viewport" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <IconDots/>
+                                    data-bs-boundary="viewport" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <IconDots />
                                 </button>
                                 <div class="dropdown-menu dropdown-menu-end">
-                                    <a @click="abrirModalAdicionarRET(item)" class="dropdown-item"
-                                       href="javascript:void(0)">Adicionar RET</a>
-                                    <a v-if="item.abio_ret" class="dropdown-item" target="_blank"
-                                       :href="route('contratos.contratada.servicos.passagem_fauna.configuracao.vincular_abio.visualizar_ret', {contrato: contrato.id, servico: servico.id, abio_ret: item.abio_ret?.id})">Visualizar
-                                        RET</a>
-                                    <a @click="excluirABIO(item.id)" class="dropdown-item"
-                                       href="javascript:void(0)">
-                                        Excluir
-                                    </a>
+                                    <span
+                                        v-if="!servico.parecer_passagem_fauna || ![1, 3].includes(servico.parecer_passagem_fauna.fk_status)">
+                                        <a @click="abrirModalAdicionarRET(item)" class="dropdown-item"
+                                            href="javascript:void(0)">Adicionar RET</a>
+                                        <a v-if="item.abio_ret" class="dropdown-item" target="_blank"
+                                            :href="route('contratos.contratada.servicos.passagem_fauna.configuracao.vincular_abio.visualizar_ret', { contrato: contrato.id, servico: servico.id, abio_ret: item.abio_ret?.id })">Visualizar
+                                            RET</a>
+                                        <a @click="excluirABIO(item.id)" class="dropdown-item"
+                                            href="javascript:void(0)">
+                                            Excluir
+                                        </a>
+                                    </span>
                                 </div>
                             </td>
                         </tr>
@@ -102,8 +105,8 @@ const excluirABIO = (abio_id) => {
             </template>
         </Navbar>
 
-        <ModalVincularABIO ref="modalVincularABIO" :contrato="contrato" :servico="servico"/>
-        <ModalAdicionarRET ref="modalAdicionarRET" :contrato="contrato" :servico="servico"/>
+        <ModalVincularABIO ref="modalVincularABIO" :contrato="contrato" :servico="servico" />
+        <ModalAdicionarRET ref="modalAdicionarRET" :contrato="contrato" :servico="servico" />
 
     </AuthenticatedLayout>
 </template>
