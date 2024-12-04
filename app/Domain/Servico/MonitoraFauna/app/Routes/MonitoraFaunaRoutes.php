af1d4a7f889ceb5fdb65f231e07ae934d4939351
Vinicius S Coelho <coelho.provnc@gmail.com>
2024-12-04 04:05:15 -0300

    feat: configuração - ECO-100

--- app/Domain/Servico/MonitoraFauna/app/Routes/MonitoraFaunaRoutes.php
+++ app/Domain/Servico/MonitoraFauna/app/Routes/MonitoraFaunaRoutes.php
@@ -7,5 +7,8 @@ Route::prefix('/monitora_fauna')->group(function () {
     Route::prefix('/vincular_abio')->group(function () {
       require __DIR__ . '../../../Configuracao/VincularABIO/Routes/VincularABIORoutes.php';
     });
+    Route::prefix('/modulo_amostral')->group(function () {
+      require __DIR__ . '../../../Configuracao/ModuloAmostral/Routes/ModuloAmostralRoutes.php';
+    });
   });
 });
