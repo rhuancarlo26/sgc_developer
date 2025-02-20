e6ff968ee9593619c205c0b78554de2aeec733de
Vinicius S Coelho <coelho.provnc@gmail.com>
2025-02-20 14:54:30 -0300

    ECO-118-1: Tela inicial PMQA

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, usePage } from '@inertiajs/vue3';
-
+import Navbar from './Dashboard/Navbar.vue';
 </script>
 
 <template>
@@ -9,11 +9,10 @@ import { Head, usePage } from '@inertiajs/vue3';
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <!-- <template #header>
-            <h2 class="m-0">Dashboard</h2>
-        </template> -->
 
+        <Navbar />
         <div class="card card-body">
+
             <h3>Bem vindo, {{ $page.props.auth.user.name }}!</h3>
             <span class="text-white">{{ usePage().props.app_url }}</span>
         </div>
---
+++ resources/js/Pages/Dashboard/Navbar.vue
@@ -0,0 +1,22 @@
+<script setup>
+import NavLink from '@/Components/NavLink.vue';
+import { IconLayoutDashboard } from '@tabler/icons-vue';
+
+</script>
+<template>
+  <header class="navbar-expand-md mb-4">
+    <div class="collapse navbar-collapse" id="navbar-menu">
+      <div class="navbar">
+        <div class="container-xl">
+          <div class="row flex-fill align-items-center">
+            <div class="col">
+              <ul class="navbar-nav">
+                <NavLink route-name="dashboard.pmqa" title="PMQA" :icon="IconLayoutDashboard" />
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
