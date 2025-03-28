571041efbb40d70c44b1475cd4a4cad3e3815e6e
Jordan <jordan.silva@gran.com>
2025-03-28 14:54:19 -0300

    logo

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,4 +1,7 @@
 <template>
     <!-- LOGO ECOSSISTEMA DNIT -->
-    <img src="/img/logo/logoecosistema (12).png" alt="Logo Ecossistema DNIT">
+    <img :src="usePage().props.app_url + `/img/logo/logoecosistema12.png`" alt="Logo Ecossistema DNIT">
 </template>
+<script setup lang="ts">
+import {usePage} from "@inertiajs/vue3";
+</script>
