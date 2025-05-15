07e705b0d11b68c247408dfde316c5eec27db8a4
Marcos Vinicius <marcosviniciuslima.2a@gmail.com>
2025-05-15 17:46:52 -0300

    novo file upload para licenca

--- app/Domain/Licenca/Documento/Router/DocumentosRouter.php
+++ app/Domain/Licenca/Documento/Router/DocumentosRouter.php
@@ -9,4 +9,9 @@ Route::prefix('documento')->group(function () {
     Route::post('store',                    [StoreLicencaDocumentoController::class, 'index'])->name('licenca.documento.store');
     Route::delete('delete/{licenca}',       [DeleteLicencaDocumentoController::class, 'index'])->name('licenca.documento.delete');
     Route::get('/visualizar/{licenca}',     [VisualizarDocumentoController::class, 'index'])->name('licenca.documento.visualizar');
+
+
+    Route::post('storeTermo',                    [StoreLicencaDocumentoController::class, 'termo'])->name('licenca.documento.termo.store');
+    Route::delete('deleteTermo/{licenca}',       [DeleteLicencaDocumentoController::class, 'termo'])->name('licenca.documento.termo.delete');
+    Route::get('/visualizarTermo/{licenca}',     [VisualizarDocumentoController::class, 'termo'])->name('licenca.documento.termo.visualizar');
 });
