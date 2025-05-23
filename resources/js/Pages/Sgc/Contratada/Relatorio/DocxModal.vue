61c5883e70cf6a680745218f7eb6825a3d0a4813
rhuancarlo26 <rhuan_borges@hotmail.com>
2025-05-23 14:47:06 -0300

    Ajuste na func por versão do relatório de coordenação

--- resources/js/Pages/Sgc/Contratada/Relatorio/DocxModal.vue
+++ resources/js/Pages/Sgc/Contratada/Relatorio/DocxModal.vue
@@ -39,7 +39,8 @@ const abrirModal = async (idItem, contratoId, versao) => {
         filePath = route('sgc.contratada.get_docx', {
             itemId: idItem,
             contratoId: contratoId,
-            versao: versao
+            versao: versao,
+            numRelatorio: props.numRelatorio
         });
         console.log('URL gerada:', filePath); 
         try {
@@ -58,19 +59,52 @@ const abrirModal = async (idItem, contratoId, versao) => {
     }
 };
 
+// const fetchDocumentos = async (itemId, contratoId, versao) => {
+//     try {
+//         const response = await fetch(route('sgc.relatorio_coordenacao_upload.index'));
+//         const data = await response.json();
+//         console.log('Dados localizados:', data);
+
+//         // Encontre o documento com a versão específica
+//         const docModal = data.find(doc => doc.item_id === itemId && doc.contrato_id === contratoId && doc.num_relatorio === props.numRelatorio && doc.versao === versao);
+//         console.log('Documento', docModal);
+
+//         if (docModal) {
+//             return docModal.caminho;
+//         } else {
+//             return null;
+//         }
+//     } catch (error) {
+//         console.error('Erro ao buscar documentos:', error);
+//         return null;
+//     }
+// };
+
 const fetchDocumentos = async (itemId, contratoId, versao) => {
     try {
-        const response = await fetch(route('sgc.relatorio_coordenacao_upload.index'));
+        // Busca os documentos usando a rota correta e passando os parâmetros necessários
+        const response = await fetch(route('sgc.relatorio_coordenacao_upload.index'), {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
         const data = await response.json();
         console.log('Dados localizados:', data);
 
-        // Encontre o documento com a versão específica
-        const docModal = data.find(doc => doc.item_id === itemId && doc.contrato_id === contratoId && doc.num_relatorio === props.numRelatorio && doc.versao === versao);
-        console.log('Documento', docModal);
+        // Filtra o documento com base no item_id, contrato_id, num_relatorio e versao
+        const docModal = data.find(doc => 
+            doc.item_id === itemId && 
+            doc.contrato_id === contratoId && 
+            doc.num_relatorio === props.numRelatorio && 
+            doc.versao === versao
+        );
+        console.log('Documento encontrado:', docModal);
 
         if (docModal) {
             return docModal.caminho;
         } else {
+            console.log('Documento não encontrado para:', { itemId, contratoId, numRelatorio: props.numRelatorio, versao });
             return null;
         }
     } catch (error) {
