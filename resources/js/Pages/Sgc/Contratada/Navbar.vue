87c6da0d7d2b8fa6b8a68e5dd046cde0a6c09e77
rhuancarlo26 <rhuan_borges@hotmail.com>
2025-05-28 11:06:19 -0300

    Ajuste Navbar

--- resources/js/Pages/Sgc/Contratada/Navbar.vue
+++ resources/js/Pages/Sgc/Contratada/Navbar.vue
@@ -29,6 +29,7 @@ const props = defineProps({
 
           <!-- Atualizar Base -->
           <navLink route-name="sgc.contratada.edicao"
+          title="Atualizar Base" :icon="IconDatabaseEdit" />
         </ul>
       </div>
       <div class="col-11">
