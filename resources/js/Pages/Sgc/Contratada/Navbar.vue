a2fc38fc60241530ba3877f2df250accb592f2b8
rhuancarlo26 <rhuan_borges@hotmail.com>
2025-05-28 11:29:49 -0300

    Correção Navbar

--- resources/js/Pages/Sgc/Contratada/Navbar.vue
+++ resources/js/Pages/Sgc/Contratada/Navbar.vue
@@ -29,6 +29,8 @@ const props = defineProps({
 
           <!-- Atualizar Base -->
           <navLink route-name="sgc.contratada.edicao"
+           title="Atualizar Base" :icon="IconDatabaseEdit" />
+
         </ul>
       </div>
       <div class="col-11">
