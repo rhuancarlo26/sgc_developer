737877d587d3175d494e4aea1bc59e80d4885704
Jordan Fernandes <jordan_silva93@hotmail.com>
2025-06-02 19:44:14 -0300

    ajustes lote de obra

--- app/Domain/Servico/MonitoraFauna/Configuracao/ModuloAmostral/Controller/StoreController.php
+++ app/Domain/Servico/MonitoraFauna/Configuracao/ModuloAmostral/Controller/StoreController.php
@@ -14,12 +14,15 @@ use Inertia\Response;
 
 class StoreController extends Controller
 {
-  public function __construct(private readonly ModuloAmostralService $moduloAmostralService) {}
+	public function __construct(private readonly ModuloAmostralService $moduloAmostralService) {}
 
-  public function index(Contrato $contrato, Servicos $servico, StoreRequest $request): RedirectResponse
-  {
-    $response = $this->moduloAmostralService->store($request->validated());
+	public function index(Contrato $contrato, Servicos $servico, StoreRequest $request): RedirectResponse
+	{
+		$response = $this->moduloAmostralService->store($request->validated());
 
-    return to_route('contratos.contratada.servicos.monitora_fauna.configuracoes.modulo_amostral.index', ['contrato' => $contrato->id, 'servico' => $servico->id])->with('message', $response['request']);
-  }
+		return to_route('contratos.contratada.servicos.monitora_fauna.configuracoes.modulo_amostral.index', [
+			'contrato' => $contrato->id,
+			'servico' => $servico->id
+		])->with('message', $response['request']);
+	}
 }
