6e68b2bc4dcae2470cd19feec00de75fb450f4b1
minoru murao silvestre <mincorp18@gmail.com>
2025-06-13 11:00:48 -0300

    hotfix/ECO-212; Erro ao Anexar Arquivo no Modal -> Cadastro de Destinação de Pilhas

--- app/Domain/Servico/SupressaoVegetacao/Execucao/Destinacao/Controller/StoreController.php
+++ app/Domain/Servico/SupressaoVegetacao/Execucao/Destinacao/Controller/StoreController.php
@@ -5,6 +5,7 @@ namespace App\Domain\Servico\SupressaoVegetacao\Execucao\Destinacao\Controller;
 use App\Domain\Servico\SupressaoVegetacao\Execucao\Destinacao\Requests\StoreRequest;
 use App\Domain\Servico\SupressaoVegetacao\Execucao\Destinacao\Services\DestinacaoService;
 use App\Shared\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 
 class StoreController extends Controller
@@ -12,15 +13,16 @@ class StoreController extends Controller
 
     public function __construct(
         private readonly DestinacaoService $destinacaoService
-    )
-    {
-    }
+    ) {}
 
-    public function __invoke(StoreRequest $request): RedirectResponse
+    public function __invoke(StoreRequest $request): JsonResponse
     {
         $response = $this->destinacaoService->store(request: $request->validated());
 
-        return redirect()->back()->with(key: 'message', value: $response['request']);
+        return response()->json([
+            'message' => 'Destinação cadastrada com sucesso.',
+            'destinacao_id' => $response['model']->id,
+            'destinacao_chave' => $response['model']->chave,
+        ]);
     }
-
 }
